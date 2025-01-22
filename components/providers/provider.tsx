49f33e007f03415e5a7484f83a830a4605088802
drCoder <drcoderjs@gmail.com>
2025-01-22 22:49:25 +0500

    initial project

---
+++ components/providers/provider.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+export const Provider = ({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) => {
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+    return (
+        <NextThemesProvider {...props}>
+            <ClerkProvider publishableKey="pk_test_...">
+                <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                    {children}
+                </ConvexProviderWithClerk>
+            </ClerkProvider>
+        </NextThemesProvider>
+    )
+};
